94679a18ff2ce1f4cd1af9a8d9c173cb4f832512
Nimi Akinroye <125094797+nimithejeagerist@users.noreply.github.com>
2026-09-16 15:27:31 -0400

    Add: string-encode-and-decode - submission-0

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-0.py
@@ -0,0 +1,23 @@
+class Solution:
+
+    def encode(self, strs: List[str]) -> str:
+        # Length prefixing
+        result = "".join(f"{len(s)}#{s}" for s in strs)
+        return result
+
+    def decode(self, s: str) -> List[str]:
+        # Two-pointer technique
+        delimiter = "#"
+        start, end = 0, 1
+        strs = []
+        while start < len(s):
+            if s[end] == delimiter:
+                length = int(s[start:end])
+                strs.append(s[end+1:end+length+1])
+                
+                # Update pointers
+                start = end + length + 1
+                end = start + 1
+            else:
+                end += 1
+        return strs
